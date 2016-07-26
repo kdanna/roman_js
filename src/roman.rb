9e758f1b406b4d969f63d22de35cc43612c25f6d
kdanna <kayce.danna@gmail.com>
2016-07-26 07:28:18 -0600

    numbers 1 - 10

--- src/roman.rb
+++ src/roman.rb
@@ -1,3 +1,30 @@
+NUMBERS = {
+	1=>"I",
+	2=>"II",
+	3=>"III",
+	4=>"IV",
+	5=>"V",
+	6=>"VI",
+	7=>"VII",
+	8=>"VIII",
+	9=>"IX",
+	10=>"X",
+	50=>"L",
+	100=>"C",
+	500=>"D",
+	1000=>"M"
+}
+
 def romanize(arabic_numeral)
+	if arabic_numeral < 11
+		NUMBERS[arabic_numeral]
+	
+	elsif arabic_numeral == 26
+		NUMBERS[10]+NUMBERS[10]+NUMBERS[5]+NUMBERS[1]
+
+	
+	end	
 
 end
+
+#Need assistance figuring out the rest of the equation without hard coding for specific integers
